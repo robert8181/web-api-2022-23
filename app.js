96a6ac77b1ea439b378662d274cdabcad679addb
unknown <a.robertcollege@gmail.com>
2022-12-20 15:34:57 +0200

    The First Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const Productrouter = require('./api/v1/routes/product');
+//
+// הוספת שכבה של ניתוב עבור מוצרים
+app.use("/product", Productrouter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
